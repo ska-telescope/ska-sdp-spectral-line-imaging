e29f3944d6df74d6d4b9fe2e2b55a43a37bb1276
Justin Jose <justinj@thoughtworks.com>
2024-11-04 19:14:53 +0530

    DHR-135: Add test for LogPluggin

--- tests/unit/ska_sdp_piper/piper/utils/test_log_util.py
+++ tests/unit/ska_sdp_piper/piper/utils/test_log_util.py
@@ -2,7 +2,7 @@ import logging
 
 from mock import mock
 
-from ska_sdp_piper.piper.utils import LogUtil
+from ska_sdp_piper.piper.utils.log_util import LogPlugin, LogUtil
 
 
 @mock.patch("ska_sdp_piper.piper.utils.log_util.configure_logging")
@@ -40,3 +40,11 @@ def test_should_configure_without_additional_log_config_if_no_output(
 def test_should_configure_verbose(configure_mock):
     LogUtil.configure(verbose=True)
     configure_mock.assert_called_once_with(level=logging.DEBUG, overrides=None)
+
+
+@mock.patch("ska_sdp_piper.piper.utils.log_util.LogUtil")
+def test_setup_worker_plugins(log_util_mock):
+    log_plugin = LogPlugin(log_file="path/to/log_file", verbose=True)
+
+    log_plugin.setup("mock_worker")
+    log_util_mock.configure.assert_called_once_with("path/to/log_file", True)
