652d3756bbb0fdc792fe87d34dc88d75e703e531
Justin Jose <justinj@thoughtworks.com>
2024-11-08 16:38:24 +0530

    WIP

--- src/ska_sdp_piper/piper/configurations/runtime_config.py
+++ src/ska_sdp_piper/piper/configurations/runtime_config.py
@@ -1,7 +1,140 @@
 from functools import reduce
 
+import yaml
+
+from ..constants import ConfigRoot
 from ..exceptions import StageNotFoundException
 from ..utils.io_utils import read_yml, write_yml
+
+
+class RuntimeConfig:
+    def __init__(self, pipeline, parameters, global_parameters):
+        """
+        Initialise the config manager object.
+
+        Parameters
+        ----------
+            pipeline: dict
+                Pipeline stages state configuration.
+            parameters: dict
+                Pipeline stages parameters configuration.
+            global_parameters: dict
+                Pipeline global parameters configuration.
+        """
+        self.pipeline = pipeline
+        self.parameters = parameters
+        self.global_parameters = global_parameters
+
+    def set(self, config_path, value):
+        """
+        Updates a given key in the config
+
+        Parameters
+        ----------
+            path: str
+                yaml/json path to the key
+            value: any
+                Value to be update with
+        """
+        root_path, *path = config_path.split(".")
+        if root_path == ConfigRoot.GLOBAL_PARAMETERS:
+            self.global_parameters = value
+
+        elif root_path == ConfigRoot.PARAMETERS:
+            if path[0] not in self.parameters:
+                raise StageNotFoundException(path[0])
+            config_param = reduce(
+                lambda acc, val: acc.get(val), path[:-1], self.parameters
+            )
+            if config_param is None:
+                raise ValueError(
+                    f"Path {'.'.join(path)} not found in configuration yaml"
+                )
+
+            config_param[path[-1]] = value
+
+        elif root_path == ConfigRoot.PIPELINE:
+            if type(value) is not bool:
+                raise ValueError(
+                    f"Stage flags need to be boolean, {type(value)} provided"
+                )
+            if path[0] not in self.pipeline:
+                raise StageNotFoundException(path[0])
+            if len(path) != 1:
+                raise ValueError("Illegal stage name parameter provided")
+
+            self.pipeline[path[0]] = value
+
+        else:
+            raise ValueError(f"Invalid configuration path {config_path}")
+
+    def update_from_yaml(self, yaml_path):
+        if not yaml_path:
+            return self
+
+        config_dict = read_yml(yaml_path)
+        self.pipeline = {
+            **self.pipeline,
+            **config_dict.get("pipeline", dict()),
+        }
+        self.parameters = {
+            **self.parameters,
+            **config_dict.get("parameters", dict()),
+        }
+        self.global_parameters = {
+            **self.global_parameters,
+            **config_dict.get("global_parameters", dict()),
+        }
+
+        return self
+
+    def update_from_cli_overrides(self, cli_overrides):
+        if not cli_overrides:
+            return self
+        params_to_update = yaml.safe_load(
+            "\n".join(f"{a} : {b}" for a, b in cli_overrides)
+        )
+
+        for (path, value) in params_to_update.items():
+            self.set(path, value)
+
+        return self
+
+    def update_from_cli_stages(self, cli_stages):
+        if not cli_stages:
+            return self
+        self.pipeline = {stage: stage in cli_stages for stage in self.pipeline}
+
+        return self
+
+    def write_yml(self, path):
+        """
+        Writes config to provided path in yaml format.
+
+        Parameters
+        ----------
+            path: str
+                Location of config file to write to.
+        """
+
+        config = {
+            "parameters": self.parameters,
+            "pipeline": self.pipeline,
+            "global_parameters": self.global_parameters,
+        }
+        write_yml(path, config)
+
+    @property
+    def stages_to_run(self):
+        """
+        Returns the stages to run.
+
+        Returns
+        -------
+        list(str)
+            List of stages names to run.
+        """
+        return [stage for stage in self.pipeline if self.pipeline.get(stage)]
 
 
 class ConfigManager:
---
+++ tests/unit/ska_sdp_piper/piper/configurations/test_runtime_config.py
@@ -0,0 +1,186 @@
+import pytest
+from mock import mock
+
+from ska_sdp_piper.piper.configurations.runtime_config import RuntimeConfig
+from ska_sdp_piper.piper.exceptions import StageNotFoundException
+
+
+@pytest.fixture(autouse=True)
+def setup():
+    pipeline = {"stage1": True, "stage2": False, "stage3": True}
+    parameters = {
+        "stage1": {"stage1_parameter_1": 0},
+        "stage2": {"stage2_parameter_1": [0, 1, 2]},
+        "stage3": {"stage3_parameter_1": 0},
+    }
+    global_parameters = {
+        "global_param_1": 1,
+        "global_param_2": 1,
+    }
+    yield RuntimeConfig(pipeline, parameters, global_parameters)
+
+
+def test_should_instantiate_config(setup):
+    pipeline = {"stage1": True, "stage2": False, "stage3": True}
+    parameters = {
+        "stage1": {"stage1_parameter_1": 0},
+        "stage2": {"stage2_parameter_1": [0, 1, 2]},
+        "stage3": {"stage3_parameter_1": 0},
+    }
+    global_parameters = {
+        "global_param_1": 1,
+        "global_param_2": 1,
+    }
+    runtime_config = setup
+
+    assert runtime_config.pipeline == pipeline
+    assert runtime_config.parameters == parameters
+    assert runtime_config.global_parameters == global_parameters
+
+
+@mock.patch("ska_sdp_piper.piper.configurations.runtime_config.read_yml")
+def test_should_update_the_default_config_with_yaml(read_yml, setup):
+    pipeline = {"stage1": False, "stage2": True, "stage3": True}
+    parameters = {
+        "stage1": {"stage1_parameter_1": 10},
+        "stage2": {"stage2_parameter_1": 20},
+    }
+    global_parameters = {
+        "global_param_1": 20,
+        "global_param_2": 25,
+    }
+
+    yaml_config = {
+        "parameters": parameters,
+        "pipeline": pipeline,
+        "global_parameters": global_parameters,
+    }
+
+    read_yml.return_value = yaml_config
+
+    runtime_config = setup
+
+    runtime_config.update_from_yaml("/path/to/yaml")
+
+    expected_parameters = {
+        "stage1": {"stage1_parameter_1": 10},
+        "stage2": {"stage2_parameter_1": 20},
+        "stage3": {"stage3_parameter_1": 0},
+    }
+
+    read_yml.assert_called_once_with("/path/to/yaml")
+
+    assert runtime_config.pipeline == pipeline
+    assert runtime_config.parameters == expected_parameters
+    assert runtime_config.global_parameters == global_parameters
+
+
+def test_should_update_pipeline_stages(setup):
+
+    pipeline_stages = ["stage3"]
+    runtime_config = setup
+
+    runtime_config.update_from_cli_stages(pipeline_stages)
+
+    assert runtime_config.pipeline == {
+        "stage1": False,
+        "stage2": False,
+        "stage3": True,
+    }
+
+
+def test_should_return_stages_to_run(setup):
+    runtime_config = setup
+    expected_stages_to_run = ["stage1", "stage3"]
+
+    assert expected_stages_to_run == runtime_config.stages_to_run
+
+
+@mock.patch("ska_sdp_piper.piper.configurations.runtime_config.write_yml")
+def test_should_write_config_to_path(write_yml_mock, setup):
+    runtime_config = setup
+
+    path = "/path/to/write"
+    config = {
+        "pipeline": {"stage1": True, "stage2": False, "stage3": True},
+        "parameters": {
+            "stage1": {"stage1_parameter_1": 0},
+            "stage2": {"stage2_parameter_1": [0, 1, 2]},
+            "stage3": {"stage3_parameter_1": 0},
+        },
+        "global_parameters": {
+            "global_param_1": 1,
+            "global_param_2": 1,
+        },
+    }
+
+    runtime_config.write_yml(path)
+
+    write_yml_mock.assert_called_once_with(path, config)
+
+
+def test_should_update_key_in_config(setup):
+    runtime_config = setup
+    runtime_config.set("parameters.stage1.stage1_parameter_1", 10)
+    runtime_config.set("parameters.stage3.stage3_parameter_1", 20)
+    runtime_config.set("global_parameters", {"env": "some-env-var"})
+    runtime_config.set("pipeline.stage1", False)
+
+    assert runtime_config.pipeline == {"stage1": False, "stage2": False, "stage3": True}
+    assert runtime_config.parameters == {
+            "stage1": {"stage1_parameter_1": 10},
+            "stage2": {"stage2_parameter_1": [0, 1, 2]},
+            "stage3": {"stage3_parameter_1": 20},
+        }
+    assert runtime_config.global_parameters == {
+        "env": "some-env-var"
+    }
+
+
+def test_should_throw_error_for_non_existent_path_in_config(setup):
+    runtime_config = setup
+    with pytest.raises(StageNotFoundException):
+        runtime_config.set(
+            "parameters.stage40.stage2_parameter_1", [10, 20, 30]
+        )
+
+
+def test_should_throw_error_for_non_existent_toplevel_path_in_config(setup):
+    runtime_config = setup
+    with pytest.raises(StageNotFoundException):
+        runtime_config.set("parameters.stage40", [10, 20, 30])
+
+
+def test_should_validate_stage_overrides(setup):
+    runtime_config = setup
+    with pytest.raises(ValueError):
+        runtime_config.set("pipeline.stage1", 10)
+
+    with pytest.raises(StageNotFoundException):
+        runtime_config.set("pipeline.stag40", True)
+
+    with pytest.raises(ValueError):
+        runtime_config.set("pipeline.stage1.param1", True)
+
+
+def test_should_throw_error_when_unknown_namespace_is_added(setup):
+    runtime_config = setup
+    with pytest.raises(ValueError):
+        runtime_config.set("x.y,z", False)
+
+
+# def test_should_be_able_to_set_param_with_default_none():
+#     pipeline = {"stage1": True}
+#     parameters = {"stage1": {"parameter": None}}
+#     global_parameters = {}
+
+#     runtime_config = RuntimeConfig(pipeline, parameters, global_parameters)
+#     runtime_config.set("parameters.stage1.parameter", 10)
+
+#     assert runtime_config.config == {
+#         "parameters": {
+#             "stage1": {"parameter": 10},
+#         },
+#         "pipeline": {"stage1": True},
+#         "global_parameters": {},
+#     }
