15c54f997368de08c6f53bc81ca0e677357eaa3e
Maneesh Sutar <maneesh.sutar@thoughtworks.com>
2024-11-13 00:11:15 +0530

    DHR-135: Add integration test for generation of dirty image

--- tests/integration/ska_sdp_spectral_line_imaging/test_pipeline.py
+++ tests/integration/ska_sdp_spectral_line_imaging/test_pipeline.py
@@ -83,3 +83,57 @@ def test_should_run_pipeline(prepare_test_data):
     expected_products.sort()
     actual_products.sort()
     assert expected_products == actual_products
+
+
+def test_should_generate_dirty_image_when_niter_major_is_zero(
+    prepare_test_data,
+):
+    """
+    Given a processing set, a config, and model FITS images
+    in desired output polarization are provided to the pipeline,
+    when number of major iterations is set to 0,
+    then the pipeline should generate only dirty image as the output of
+    imaging stage.
+    """
+    time_stamp = timestamp()
+    output_dir = f"{prepare_test_data}/pipeline_output"
+
+    testargs = [
+        "test",
+        "run",
+        "--input",
+        MSIN,
+        "--output",
+        output_dir,
+        "--config",
+        "./test.config.yml",
+        "--set",
+        "parameters.imaging.n_iter_major",
+        "0",
+        "--set",
+        "parameters.continuum_subtraction.export_residual",
+        "false",
+        "--set",
+        "parameters.predict_stage.export_model",
+        "false",
+    ]
+    with mock.patch.object(sys, "argv", testargs):
+        spectral_line_imaging_pipeline()
+
+    pipeline_output_path = (
+        f"{output_dir}/spectral_line_imaging_pipeline_{time_stamp}"
+    )
+    assert os.path.exists(pipeline_output_path)
+
+    expected_products = [
+        # TODO: model and residual exports can be turned off
+        # for other tests as it is already convered once
+        "test_cube.dirty.fits",
+        f"spectral_line_imaging_pipeline_{timestamp()}.log",
+        f"spectral_line_imaging_pipeline_{timestamp()}.config.yml",
+        f"spectral_line_imaging_pipeline_{timestamp()}.cli.yml",
+    ]
+    actual_products = os.listdir(pipeline_output_path)
+    expected_products.sort()
+    actual_products.sort()
+    assert expected_products == actual_products
