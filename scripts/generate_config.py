410a7035bc0051b502a9d35a139f101579b48438
Maneesh Sutar <maneesh.sutar@thoughtworks.com>
2024-11-18 17:52:27 +0530

    DHR-135: Fix the script to auto generate stage config

--- scripts/generate_config.py
+++ scripts/generate_config.py
@@ -1,126 +1,52 @@
+from functools import reduce
+
 import pandas as pd
 
+from ska_sdp_piper.piper.configurations.nested_config import NestedConfigParam
 from ska_sdp_spectral_line_imaging import pipeline
+
 
 ############################################
 # Creating dictionary of dataframes
 # Each dataframe contains Configuration info
 ############################################
+def process_config_param(prefix, config_param):
+    if config_param._type is NestedConfigParam:
+        return reduce(
+            lambda acc, param: [
+                *acc,
+                *process_config_param(f"{prefix}.{param[0]}", param[1]),
+            ],
+            config_param._config_params.items(),
+            [],
+        )
+
+    return [{"param": prefix, **config_param.__dict__}]
+
+
 dfs = {}
+
 for x in pipeline.spectral_line_imaging_pipeline._stages:
-    df = pd.DataFrame(
-        [
-            {"param": k, **v.__dict__}
-            for k, v in x._Stage__config._Configuration__config_params.items()
-        ]
-    ).fillna("None")
+    df = []
+    for name, config_param in x._Stage__config._config_params.items():
+        df.extend(process_config_param(name, config_param))
+    df = pd.DataFrame(df).fillna("None")
     if df.empty:
         continue
     df = df.rename(columns={"_type": "type"})
+    df = df.rename(columns={"_ConfigParam__value": "default"})
+    df = df.rename(columns={"allowed_values": "allowed values"})
     df.columns = df.columns.str.capitalize()
     df["Type"] = df["Type"].apply(lambda x: x.__name__)
+    df["Allowed values"] = df["Allowed values"].apply(
+        lambda x: "" if x == "None" else x
+    )
     dfs[x.name] = df
-
-
-###############################
-# Handing imaging configuration
-###############################
-
-# Since imaging stage config contains dictionaries inside each ConfigParam,
-# the key value pairs inside dictionary have to be extracted out seperately
-image_df = dfs["imaging"]
-new_rows = []
-params_to_remove = []
-
-# Iterate over the rows of the original DataFrame
-for _, row in image_df.iterrows():
-    param = row["Param"]
-    default = row["Default"]
-
-    if isinstance(default, dict):
-        params_to_remove.append(param)
-        # Iterate over the dictionary items in the 'Default' column
-        for key, value in default.items():
-            new_param = f"{param}.{key}"  # Combine the Param and key
-            if value is None:
-                value = "None"
-            new_rows.append(
-                {
-                    "Param": new_param,
-                    "Type": type(value).__name__,
-                    "Default": value,
-                    "Description": "",
-                }
-            )
-
-new_df = pd.DataFrame(new_rows)
-
-for name in params_to_remove:
-    image_df = image_df[image_df["Param"] != name]
-
-image_df = pd.concat([image_df, new_df], ignore_index=True)
-
-# Add missing data
-
-image_df.loc[
-    image_df["Param"] == "gridding_params.cell_size", "Type"
-] = "float"
-image_df.loc[
-    image_df["Param"] == "gridding_params.cell_size", "Description"
-] = "Cell size of the image in arcseconds"
-image_df.loc[
-    image_df["Param"] == "gridding_params.scaling_factor", "Description"
-] = "Scaling factor used for esimation of cell size."
-image_df.loc[
-    image_df["Param"] == "gridding_params.epsilon", "Description"
-] = "Floating point accuracy for ducc gridder"
-image_df.loc[
-    image_df["Param"] == "gridding_params.image_size", "Description"
-] = "Spatial size (nx and ny) of the spectral cube"
-
-image_df.loc[
-    image_df["Param"] == "deconvolution_params.algorithm", "Description"
-] = "Algorithm for deconvolution. If use_radler is False, then options are 'hogbom'|'msclean'. With radler, the options are 'multiscale'|'iuwt'|'more_sane'|'generic_clean'"
-image_df.loc[
-    image_df["Param"] == "deconvolution_params.gain", "Description"
-] = "Loop gain"
-image_df.loc[
-    image_df["Param"] == "deconvolution_params.threshold", "Description"
-] = "Clean threshold"
-image_df.loc[
-    image_df["Param"] == "deconvolution_params.fractional_threshold",
-    "Description",
-] = "Fractional threshold"
-image_df.loc[
-    image_df["Param"] == "deconvolution_params.scales", "Description"
-] = "Scales in pixels for multiscale"
-image_df.loc[
-    image_df["Param"] == "deconvolution_params.niter", "Description"
-] = "Maximum number of minor cycle iterations"
-image_df.loc[
-    image_df["Param"] == "deconvolution_params.use_radler", "Description"
-] = "Whether to use radler or not"
-
-image_df.loc[image_df["Param"] == "beam_info.bmaj", "Type"] = "float"
-image_df.loc[
-    image_df["Param"] == "beam_info.bmaj", "Description"
-] = "Beam major axis in degrees"
-image_df.loc[image_df["Param"] == "beam_info.bmin", "Type"] = "float"
-image_df.loc[
-    image_df["Param"] == "beam_info.bmin", "Description"
-] = "Beam minor axis in degrees"
-image_df.loc[image_df["Param"] == "beam_info.bpa", "Type"] = "float"
-image_df.loc[
-    image_df["Param"] == "beam_info.bpa", "Description"
-] = "Beam position angle in degrees"
-
-dfs["imaging"] = image_df
 
 
 #######################
 # Generate the RST file
 #######################
-
 header = """Stage Configs
 =============
 
@@ -133,7 +59,7 @@ header = """Stage Configs
 table_config = """
 ..  table::
     :width: 100%
-    :widths: 25, 10, 20, 45
+    :widths: 15, 10, 10, 45, 10, 10
 """
 
 indent = "    "
